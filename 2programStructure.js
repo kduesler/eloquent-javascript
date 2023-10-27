745f348a23f393d112c8fe659e914eaab600c199
kduesler <kyle.duesler.09@gmail.com>
2023-10-27 03:45:05 -0500

    Chessboard

--- 2programStructure.js
+++ 2programStructure.js
@@ -1,24 +1,44 @@
-//EXERCISES
+// //EXERCISES
 
-//Looping a Triangle
-let x = '#'
-let y = '#'
+// //Looping a Triangle
+// let x = '#'
+// let y = '#'
 
-while (y.length < 8) {
-  console.log(y);
-  y = y+x;
+// while (y.length < 8) {
+//   console.log(y);
+//   y = y+x;
+// }
+
+
+// //FizzBuzz
+// let number = 0
+// for (let index = 0; index <= 100; index++) {
+//   if (number % 3 === 0 && number % 5 === 0) {
+//     console.log("FIZZBUZZ");
+//   } else if (number % 5 === 0){
+//     console.log("BUZZ");
+//   } else if (number % 3 === 0 ) {
+//     console.log("FIZZ");
+//   } else console.log(number);
+//   number ++
+// }
+
+//Chessboard
+
+size = 80;
+board = "";
+for (let index2 = 0; index2 < size; index2++) {
+for (let index1 = 0; index1 < size; index1++) {
+if (board.length % 2 === 0 ) {
+  board += " "
+} else  {
+  board += "#"
 }
-
-
-//FizzBuzz
-let number = 0
-for (let index = 0; index <= 100; index++) {
-  if (number % 3 === 0 && number % 5 === 0) {
-    console.log("FIZZBUZZ");
-  } else if (number % 5 === 0){
-    console.log("BUZZ");
-  } else if (number % 3 === 0 ) {
-    console.log("FIZZ");
-  } else console.log(number);
-  number ++
 }
+board += "\n"
+}
+console.log(board);
+
+
+
+
